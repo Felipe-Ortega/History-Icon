365d4f7a715134716727bf061b310f2fbcf0c23f
Felipe-Ortega <felipe.osouza@sptech.school>
2024-11-15 22:16:42 -0300

    Banco Atualizado

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,62 +1,50 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+CREATE DATABASE HistoryIcon;
+USE HistoryIcon;
 
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+CREATE TABLE Usuario (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    email VARCHAR(45),
+    senha VARCHAR(45),
+    ativo char(1)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE Log (
+    idLog INT PRIMARY KEY AUTO_INCREMENT,
+    dtHrAcesso DATETIME,
+    fkUsuario INT,
+    FOREIGN KEY (fkUsuario) REFERENCES Usuario(idUsuario)
+);
+CREATE TABLE Pergunta(
+	idPergunta INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(100),
+    respostaCorreta VARCHAR(45)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE Resposta (
+    idResposta INT AUTO_INCREMENT,
+    fkUsuario INT,
+    fkPergunta INT,
+    resposta VARCHAR(45),
+    isCorreta CHAR(1),
+    CONSTRAINT fkUsuarioResposta foreign key (fkUsuario) references usuario(idUsuario),
+    CONSTRAINT fkPerguntaResposta foreign key (fkPergunta) references pergunta(idPergunta),
+    CONSTRAINT pkResposta primary key (idResposta, fkUsuario, fkPergunta)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+INSERT INTO Pergunta VALUES 
+(DEFAULT, "Quem criou a teoria da relatividade?", "Albert Einsten"),
+(DEFAULT, "Qual o nome do guerreiro que inspirou a maratona?", "Fidípides"),
+(DEFAULT, "Quem foi o matemático a chegar mais próximo de PI na idade antiga?", "Arquemedes"),
+(DEFAULT, "Qual a velocidade da luz no vácuo?", "300.000.000 m/s"),
+(DEFAULT, "Quem foi o primeiro astrônomo a dizer que a Terra gira em torno do Sol?", "Galileu Galilei"),
+(DEFAULT, "Caso o Sol suma repentinamente, quanto tempo demoraria para perceber-mos?", "8,5 minutos"),
+(DEFAULT, "Quem originou a ideia de que 2 corpos não ocupam o mesmo espaço?", "Isaac Newton"),
+(DEFAULT, "Quem é o pai da filosofia?", "Tales de Mileto"),
+(DEFAULT, "Quem criou a tabela periódica atual?", "Dmitri Mendeleiev"),
+(DEFAULT, "Quem foi a primeira pessoa a escrever um algoritmo?", "Ada Lovelace");
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+select * from usuario;
+select * from resposta;
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+SELECT COUNT(isCorreta) FROM resposta WHERE isCorreta = 'S';
